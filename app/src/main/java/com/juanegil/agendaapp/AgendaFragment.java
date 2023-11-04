31e3b8a57d8d8d81fcc147622e35913aa20a5b49
Juan E. Gil <JuanE.Gil2002@Gmail.com>
2023-11-04 12:49:34 -0500

    30. Obtener Citas y separarlas por Día

--- app/src/main/java/com/juanegil/agendaapp/AgendaFragment.java
+++ app/src/main/java/com/juanegil/agendaapp/AgendaFragment.java
@@ -1,5 +1,6 @@
 package com.juanegil.agendaapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +11,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.juanegil.agendaapp.adapter.CitasDiaAdapter;
+import com.juanegil.agendaapp.database.AppDatabase;
 import com.juanegil.agendaapp.databinding.FragmentAgendaBinding;
 import com.juanegil.agendaapp.models.Cita;
+import com.juanegil.agendaapp.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +24,16 @@ public class AgendaFragment extends Fragment {
 
     FragmentAgendaBinding binding;
     List<Cita> listaCitas = new ArrayList<>();
+    List<Cita> listaCitasLunes = new ArrayList<>();
+    List<Cita> listaCitasMartes = new ArrayList<>();
+    List<Cita> listaCitasMiercoles = new ArrayList<>();
+    List<Cita> listaCitasJueves = new ArrayList<>();
+    List<Cita> listaCitasViernes = new ArrayList<>();
+    List<Cita> listaCitasSabado = new ArrayList<>();
+    List<Cita> listaCitasDomingo = new ArrayList<>();
     CitasDiaAdapter citasDiaAdapter;
+
+    AppDatabase db;
 
     public AgendaFragment() {
         // Required empty public constructor
@@ -47,7 +59,64 @@ public class AgendaFragment extends Fragment {
 
         ((MainActivity) getActivity()).getSupportActionBar().hide();
 
+        db = new Utils().getAppDatabase(getContext());
+
+        obtenerTodasCitas();
+
         return vista;
+    }
+
+    private void obtenerTodasCitas() {
+        AsyncTask.execute(() -> {
+            listaCitas = db.citaDao().obtenerCitas();
+            getActivity().runOnUiThread(() -> {
+                obtenerCitasDia();
+            });
+        });
+    }
+
+    private void obtenerCitasDia() {
+        for (Cita cita : listaCitas) {
+            switch (cita.diaCita) {
+                case "Lunes":
+                    listaCitasLunes.add(cita);
+                    setupRecyclerView(binding.rvCitasLunes, listaCitasLunes);
+                    break;
+
+                case "Martes":
+                    listaCitasMartes.add(cita);
+                    setupRecyclerView(binding.rvCitasMartes, listaCitasMartes);
+                    break;
+
+                case "Miercoles":
+                    listaCitasMiercoles.add(cita);
+                    setupRecyclerView(binding.rvCitasMiercoles, listaCitasMiercoles);
+                    break;
+
+                case "Jueves":
+                    listaCitasJueves.add(cita);
+                    setupRecyclerView(binding.rvCitasJueves, listaCitasJueves);
+                    break;
+
+                case "Viernes":
+                    listaCitasViernes.add(cita);
+                    setupRecyclerView(binding.rvCitasViernes, listaCitasViernes);
+                    break;
+
+                case "Sabado":
+                    listaCitasSabado.add(cita);
+                    setupRecyclerView(binding.rvCitasSabado, listaCitasSabado);
+                    break;
+
+                case "Domingo":
+                    listaCitasDomingo.add(cita);
+                    setupRecyclerView(binding.rvCitasDomingo, listaCitasDomingo);
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 
     private void setupRecyclerView(RecyclerView rvDia, List<Cita> listaDia) {
